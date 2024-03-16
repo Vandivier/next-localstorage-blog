92fb7aeb9fdefdb1a0c15b7acf6e84d3443f3653
Vandivier <vandivi3r@gmail.com>
2024-03-16 12:46:16 -0400

    feat: sign up link

--- next-localstorage-blog/app/page.tsx
+++ next-localstorage-blog/app/page.tsx
@@ -26,6 +26,9 @@ export default function Home() {
           <li>
             <a href="/about">About</a>
           </li>
+          <li>
+            <a href="/sign-up">Sign Up</a>
+          </li>
         </ul>
       </nav>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
